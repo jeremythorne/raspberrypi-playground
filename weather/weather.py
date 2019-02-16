10d902f905b29c4386894fa701ba2a1ca38745b2
Jeremy Thorne <jeremythorne@github.com>
2019-02-16 19:09:19 +0000

    add Met Office DataPoint weather

--- weather/weather.py
+++ weather/weather.py
@@ -11,11 +11,14 @@ lon="0.16282"
 url_base="http://api.openweathermap.org/data/2.5/"
 
 url = "{0}forecast?lat={1}&lon={2}&units=metric&APPID={3}".format(url_base, lat, lon, app_id)
-
+print(url)
 r = requests.get(url)
+if r.status_code != 200:
+    print("error {}: {}".format(r.status_code, r.text))
 j = r.json()
 city = j['city']['name']
 now = j['list'][0]
 temp = now['main']['temp']
 weather = now['weather'][0]['description']
-print("the weather forecast for {0} for the next three hours is {1}, temp {2} C".format(city, weather, temp))
+date_txt = now['dt_txt']
+print("the weather forecast for {} for the next three hours ({}) is {}, temp {} C".format(city, date_txt, weather, temp))
