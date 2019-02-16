cc835ed80601beba1c30c01812db691d39300b10
Jeremy Thorne <jeremythorne@github.com>
2019-02-16 20:04:00 +0000

    simple rendering of weather to image
    in preparation for display on pimoroni inky phat

---
+++ weather/metoffice_weather_img.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+
+from font_fredoka_one import FredokaOne
+from PIL import ImageFont, Image, ImageDraw
+import requests
+
+app_id = open("/home/pi/metofficekey.txt").read().strip()
+
+location="350107" #Anglesey Abbey
+url_base="http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/"
+
+url = "{}{}?res=3hourly&key={}".format(url_base, location, app_id)
+print(url)
+r = requests.get(url)
+if r.status_code != 200:
+    print("error {}: {}".format(r.status_code, r.text))
+j = r.json()
+
+weather_types = {
+        "NA" : "Not available",
+        "0"  : "Clear night",
+        "1"  : "Sunny day",
+        "2"  : "Partly cloudy (night)",
+        "3"  : "Partly cloudy (day)",
+        "4"  : "Not used",
+        "5"  : "Mist",
+        "6"  : "Fog",
+        "7"  : "Cloudy",
+        "8"  : "Overcast",
+        "9"  : "Light rain shower (night)",
+        "10" : "Light rain shower (day)",
+        "11" : "Drizzle",
+        "12" : "Light rain",
+        "13" : "Heavy rain shower (night)",
+        "14" : "Heavy rain shower (day)",
+        "15" : "Heavy rain",
+        "16" : "Sleet shower (night)",
+        "17" : "Sleet shower (day)",
+        "18" : "Sleet",
+        "19" : "Hail shower (night)",
+        "20" : "Hail shower (day)",
+        "21" : "Hail",
+        "22" : "Light snow shower (night)",
+        "23" : "Light snow shower (day)",
+        "24" : "Light snow",
+        "25" : "Heavy snow shower (night)",
+        "26" : "Heavy snow shower (day)",
+        "27" : "Heavy snow",
+        "28" : "Thunder shower (night)",
+        "29" : "Thunder shower (day)",
+        "30" : "Thunder",
+        }
+
+
+loc = j['SiteRep']['DV']['Location']
+city = loc['name']
+now = loc['Period'][0]
+temp = now['Rep'][0]['F'] # F = Feels like
+
+min_temp = int(temp)
+max_temp = min_temp
+for rep in now['Rep']:
+    F = int(rep['F'])
+    max_temp = F if F > max_temp else max_temp
+    min_temp = F if F < min_temp else min_temp
+
+max_temp = str(max_temp)
+min_temp = str(min_temp)
+
+weather = weather_types[now['Rep'][0]['W']]
+date_txt = now['value'][:-1]
+print("the weather forecast for {} ({}) is {}, temp {} ({}/{}) C".format(city, date_txt, weather, temp, min_temp, max_temp))
+
+font = ImageFont.truetype(FredokaOne, 22)
+mini_font = ImageFont.truetype(FredokaOne, 16)
+W, H = 212, 104
+white = (255, 255, 255)
+img = Image.new("RGB", (W, H))
+draw = ImageDraw.Draw(img)
+draw.text((10, 10), date_txt, font=font, fill=white)
+draw.text((10, 40), weather, font=font, fill=white)
+temp_str = "{}C".format(temp)
+draw.text((10, 70), temp_str, font=font, fill=white)
+w, h = font.getsize(temp_str)
+mxw, mxh = mini_font.getsize(max_temp)
+mnw, mnh = mini_font.getsize(min_temp)
+draw.text((10 + w + 10, 70), max_temp, font=mini_font, fill=white)
+draw.text((10 + w + 10 + mxw + 5, 70 + h - mnh), min_temp, font=mini_font, fill=white)
+
+
+img.save("weather.png")
