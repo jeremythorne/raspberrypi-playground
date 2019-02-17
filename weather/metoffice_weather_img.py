484db6c0048a89ef86811436606d8a9cf145eb5f
Jeremy Thorne <jeremythorne@github.com>
2019-02-17 16:18:56 +0000

    shorter weather descriptions

--- weather/metoffice_weather_img.py
+++ weather/metoffice_weather_img.py
@@ -30,37 +30,37 @@ class Weather:
 
         weather_types = {
                 "NA": "Not available",
-                "0": "Clear night",
-                "1": "Sunny day",
-                "2": "Partly cloudy (night)",
-                "3": "Partly cloudy (day)",
-                "4": "Not used",
-                "5": "Mist",
-                "6": "Fog",
-                "7": "Cloudy",
-                "8": "Overcast",
-                "9": "Light rain shower (night)",
-                "10": "Light rain shower (day)",
-                "11": "Drizzle",
-                "12": "Light rain",
-                "13": "Heavy rain shower (night)",
-                "14": "Heavy rain shower (day)",
-                "15": "Heavy rain",
-                "16": "Sleet shower (night)",
-                "17": "Sleet shower (day)",
-                "18": "Sleet",
-                "19": "Hail shower (night)",
-                "20": "Hail shower (day)",
-                "21": "Hail",
-                "22": "Light snow shower (night)",
-                "23": "Light snow shower (day)",
-                "24": "Light snow",
-                "25": "Heavy snow shower (night)",
-                "26": "Heavy snow shower (day)",
-                "27": "Heavy snow",
-                "28": "Thunder shower (night)",
-                "29": "Thunder shower (day)",
-                "30": "Thunder",
+                "0": ("Clear", "night"),
+                "1": ("Sunny", "day"),
+                "2": ("Partly cloudy", "night"),
+                "3": ("Partly cloudy", "day"),
+                "4": ("Not used", ""),
+                "5": ("Mist", ""),
+                "6": ("Fog", ""),
+                "7": ("Cloudy", ""),
+                "8": ("Overcast", ""),
+                "9": ("Light rain shower", "night"),
+                "10": ("Light rain shower", "day"),
+                "11": ("Drizzle", ""),
+                "12": ("Light rain", ""),
+                "13": ("Heavy rain shower", "night"),
+                "14": ("Heavy rain shower", "day"),
+                "15": ("Heavy rain", ""),
+                "16": ("Sleet shower", "night"),
+                "17": ("Sleet shower", "day"),
+                "18": ("Sleet", ""),
+                "19": ("Hail shower", "night"),
+                "20": ("Hail shower", "day"),
+                "21": ("Hail", ""),
+                "22": ("Light snow shower", "night"),
+                "23": ("Light snow shower", "day"),
+                "24": ("Light snow", ""),
+                "25": ("Heavy snow shower", "night"),
+                "26": ("Heavy snow shower", "day"),
+                "27": ("Heavy snow", ""),
+                "28": ("Thunder shower", "night"),
+                "29": ("Thunder shower", "day"),
+                "30": ("Thunder", ""),
                 }
 
         loc = j['SiteRep']['DV']['Location']
@@ -77,8 +77,8 @@ class Weather:
 
         self.max_temp = str(max_temp)
         self.min_temp = str(min_temp)
-
-        self.weather = weather_types[now['Rep'][0]['W']]
+        W = now['Rep'][0]['W']
+        self.weather = weather_types[W][0]
         self.date_txt = now['value'][:-1]
         print("""the weather forecast for {} ({})
         is {}, temp {} ({}/{}) C""".format(
@@ -91,7 +91,7 @@ class Weather:
 
     def draw(self, filename):
         """render weather to image"""
-        font = ImageFont.truetype(FredokaOne, 22)
+        font = ImageFont.truetype(FredokaOne, 20)
         mini_font = ImageFont.truetype(FredokaOne, 16)
         W, H = 212, 104
         white = (255, 255, 255)
