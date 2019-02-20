b2224cc1f04aa81cef9c7e89c39bec076be859b1
Jeremy Thorne <jeremythorne@github.com>
2019-02-20 22:24:07 +0000

    add weather icons designed by S

--- weather/metoffice_weather_img.py
+++ weather/metoffice_weather_img.py
@@ -2,6 +2,7 @@
 
 from font_fredoka_one import FredokaOne
 from PIL import ImageFont, Image, ImageDraw
+import os.path
 import requests
 import sys
 inky_available = False
@@ -40,38 +41,38 @@ class Weather:
         j = r.json()
 
         weather_types = {
-                "NA": "Not available",
-                "0": ("Clear", "night"),
-                "1": ("Sunny", "day"),
-                "2": ("Partly cloudy", "night"),
-                "3": ("Partly cloudy", "day"),
-                "4": ("Not used", ""),
-                "5": ("Mist", ""),
-                "6": ("Fog", ""),
-                "7": ("Cloudy", ""),
-                "8": ("Overcast", ""),
-                "9": ("Light rain shower", "night"),
-                "10": ("Light rain shower", "day"),
-                "11": ("Drizzle", ""),
-                "12": ("Light rain", ""),
-                "13": ("Heavy rain shower", "night"),
-                "14": ("Heavy rain shower", "day"),
-                "15": ("Heavy rain", ""),
-                "16": ("Sleet shower", "night"),
-                "17": ("Sleet shower", "day"),
-                "18": ("Sleet", ""),
-                "19": ("Hail shower", "night"),
-                "20": ("Hail shower", "day"),
-                "21": ("Hail", ""),
-                "22": ("Light snow shower", "night"),
-                "23": ("Light snow shower", "day"),
-                "24": ("Light snow", ""),
-                "25": ("Heavy snow shower", "night"),
-                "26": ("Heavy snow shower", "day"),
-                "27": ("Heavy snow", ""),
-                "28": ("Thunder shower", "night"),
-                "29": ("Thunder shower", "day"),
-                "30": ("Thunder", ""),
+                "NA":("Not available", "", ""),
+                "0": ("Clear", "night", "sunny"),
+                "1": ("Sunny", "day", "sunny"),
+                "2": ("Partly cloudy", "night", "cloudy"),
+                "3": ("Partly cloudy", "day", "cloudy"),
+                "4": ("Not used", "", ""),
+                "5": ("Mist", "", "fog"),
+                "6": ("Fog", "", "fog"),
+                "7": ("Cloudy", "", "cloudy"),
+                "8": ("Overcast", "", "cloudy"),
+                "9": ("Light rain shower", "night", "rain"),
+                "10": ("Light rain shower", "day", "rain"),
+                "11": ("Drizzle", "", "rain"),
+                "12": ("Light rain", "", "rain"),
+                "13": ("Heavy rain shower", "night", "rain"),
+                "14": ("Heavy rain shower", "day", "rain"),
+                "15": ("Heavy rain", "", "rain"),
+                "16": ("Sleet shower", "night", "rain"),
+                "17": ("Sleet shower", "day", "rain"),
+                "18": ("Sleet", "", "rain"),
+                "19": ("Hail shower", "night", "rain"),
+                "20": ("Hail shower", "day", "rain"),
+                "21": ("Hail", "", "rain"),
+                "22": ("Light snow shower", "night", "snow"),
+                "23": ("Light snow shower", "day", "snow"),
+                "24": ("Light snow", "", "snow"),
+                "25": ("Heavy snow shower", "night", "snow"),
+                "26": ("Heavy snow shower", "day", "snow"),
+                "27": ("Heavy snow", "", "snow"),
+                "28": ("Thunder shower", "night", "thunder"),
+                "29": ("Thunder shower", "day", "thunder"),
+                "30": ("Thunder", "", "thunder"),
                 }
 
         loc = j['SiteRep']['DV']['Location']
@@ -90,6 +91,7 @@ class Weather:
         self.min_temp = str(min_temp)
         W = now['Rep'][0]['W']
         self.weather = weather_types[W][0]
+        self.icon = weather_types[W][2]
         t = j['SiteRep']['DV']['dataDate']
         date, time = t.split("T")
         time = time[:-1]
@@ -112,29 +114,34 @@ class Weather:
         white = props.WHITE
         red = props.RED
         draw = ImageDraw.Draw(img)
-        draw.rectangle([(0, 0), (W, H)], black)
-        draw.arc(
+        draw.rectangle([(0, 0), (W, H)], white)
+        icon_name = self.icon + ".png"
+        if os.path.exists(icon_name):
+            icon = Image.open(icon_name)
+            img.paste(icon, (W//2, 0))
+        else:
+            draw.arc(
                 [W * 0.8, H/2 - W/2, W * 0.8 + W, H/2 + W/2],
-                0, 360, fill=red)
+                0, 360, fill=black)
         if self.error:
             draw.text((10, 10), "couldn't get forecast")
             return
         offy = 10
-        draw.text((10, offy), self.date_txt, font=mini_font, fill=white)
+        draw.text((10, offy), self.date_txt, font=mini_font, fill=red)
         w, h = mini_font.getsize(self.date_txt)
         offy += h + 5
-        draw.text((10, offy), self.weather, font=font, fill=white)
+        draw.text((10, offy), self.weather, font=font, fill=red)
         w, h = font.getsize(self.weather)
         offy += h + 5
         temp_str = "{}°".format(self.temp)
-        draw.text((10, offy), temp_str, font=big_font, fill=white)
+        draw.text((10, offy), temp_str, font=big_font, fill=red)
         w, h = big_font.getsize(temp_str)
         mxw, mxh = mini_font.getsize(self.max_temp)
         mnw, mnh = mini_font.getsize(self.min_temp)
         draw.text((10 + w + 10, offy - 2), self.max_temp,
-                  font=mini_font, fill=white)
+                  font=mini_font, fill=red)
         draw.text((10 + w + 10 + mxw / 2 - mnw / 2, offy + h - mnh + 3),
-                  self.min_temp, font=mini_font, fill=white)
+                  self.min_temp, font=mini_font, fill=red)
 
     def draw_inky(self):
         inky_display = InkyPHAT("red")
