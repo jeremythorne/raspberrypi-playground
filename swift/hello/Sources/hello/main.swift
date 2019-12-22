ab76bdc6178829666647606744b35563f05b69b3
Jeremy Thorne <jeremythorne@github.com>
2019-12-22 13:18:01 +0000

    bouncing square.
    Draws a sacled quad and bounces it round the screen

--- swift/hello/Sources/hello/main.swift
+++ swift/hello/Sources/hello/main.swift
@@ -1,3 +1,9 @@
+// borrows ideas from
+// www.swiftgl.org - OpenGL tutorial in Swift
+// www.glfw.org - cross platform GL window toolkit
+// github.com/sakrist/Swift_OpenGL_Example - iOS, Linux, Android OpenGL is Swift 
+// pygame-zero.readthedocs.io - simple python game framework
+
 import GL
 import GLFW
 
@@ -19,18 +25,21 @@ func key_callback(window: Optional<OpaquePointer>,
 }
 
 var vertices: [GLfloat] = [
-    -0.6, -0.4, 1.0, 0.0, 0.0,
-    0.6,  -0.4, 0.0, 1.0, 0.0,
-    0.0,   0.6, 0.0, 0.0, 1.0
+     0.0,  1.0, 0.0, 1.0, 0.0,
+     0.0,  0.0, 1.0, 0.0, 0.0,
+     1.0,  1.0, 0.0, 0.0, 1.0,
+     1.0,  0.0, 0.0, 0.0, 1.0,
 ]
 
 var vertex_shader_text = "#version 110\n"
 + "attribute vec3 col;\n"
 + "attribute vec2 pos;\n"
++ "uniform vec2 scale;\n"
++ "uniform vec2 offset;\n"
 + "varying vec3 color;\n"
 + "void main()\n"
 + "{\n"
-+ "  gl_Position = vec4(pos, 0.0, 1.0);\n"
++ "  gl_Position = vec4(pos * scale + offset, 0.0, 1.0);\n"
 + "  color = col;\n"
 + "}\n"
 
@@ -111,36 +120,81 @@ func main()
         print("linked")
     }
 
-    let pos_location:GLuint = GLuint(glGetAttribLocation(program, "pos"))
-    let col_location:GLuint = GLuint(glGetAttribLocation(program, "col"))
+    let pos_location = GLuint(glGetAttribLocation(program, "pos"))
+    let col_location = GLuint(glGetAttribLocation(program, "col"))
+    let scale_location = GLint(glGetUniformLocation(program, "scale")) 
+    let offset_location = GLint(glGetUniformLocation(program, "offset")) 
 
     glEnableVertexAttribArray(pos_location)
     glVertexAttribPointer(pos_location, 2, GLenum(GL_FLOAT), GLboolean(GL_FALSE),
-                            GLsizei(MemoryLayout<GLfloat>.size),
+                            GLsizei(MemoryLayout<GLfloat>.size) * 5,
                                        UnsafeRawPointer(bitPattern: 0))
     glEnableVertexAttribArray(col_location)
     glVertexAttribPointer(col_location, 3, GLenum(GL_FLOAT), GLboolean(GL_FALSE),
-                            GLsizei(MemoryLayout<GLfloat>.size),
+                            GLsizei(MemoryLayout<GLfloat>.size) * 5,
                                        UnsafeRawPointer(bitPattern: MemoryLayout<GLfloat>.size * 2))
-    var width: Int32 = 0
-    var height: Int32 = 0
-    glfwGetFramebufferSize(window, &width, &height)
-    glViewport(0, 0, width, height)
+    var iwidth: Int32 = 0
+    var iheight: Int32 = 0
+    glfwGetFramebufferSize(window, &iwidth, &iheight)
+    glViewport(0, 0, iwidth, iheight)
+    let width = Float(iwidth)
+    let height = Float(iheight)
     glClearColor(1.0, 1.0, 0.0, 1.0)
+
+    func drawRectCentered(x:Float, y:Float, w:Float, h:Float) {
+        var scale: [GLfloat] = [ w * 2.0 / width, h * 2.0 / height ]
+        var offset: [GLfloat] = [ ( (x - w / 2.0) * 2.0 / width ) - 1.0,
+                                  ( (y - h / 2.0) * 2.0 / height ) - 1.0 ]
+        glUseProgram(program)
+        glUniform2fv(scale_location, 1, &scale)
+        glUniform2fv(offset_location, 1, &offset)
+        glDrawArrays(GLenum(GL_TRIANGLE_STRIP), 0, 4)
+    }
+
+    var gx:Float = width / 2.0
+    var gy:Float = height / 2.0
+    var vx:Float = 3.0
+    var vy:Float = 2.0
+    let hs:Float = 50.0
+    func update() {
+        gx += vx
+        gy += vy
+        if gx > width - hs {
+            gx = width - hs
+            vx = -vx
+        } else if gx < hs {
+            gx = hs
+            vx = -vx
+        }
+
+        if gy > height - hs {
+            gy = height - hs
+            vy = -vy
+        } else if gy < hs {
+            gy = hs
+            vy = -vy
+        }
+    }
+
+    func draw() {
+        drawRectCentered(x:gx, y:gy, w: 2.0 * hs, h: 2.0 * hs)
+    }
 
     print("starting loop")
 
     while glfwWindowShouldClose(window) == 0 {
+        update()
+    
         glClear(GLbitfield(GL_COLOR_BUFFER_BIT))
     
-        glUseProgram(program)
-        glDrawArrays(GLenum(GL_TRIANGLES), 0, 3)
+        draw()
 
         glfwSwapBuffers(window)
         glfwPollEvents()
     }
 
 }
+
 
 print("Hello, world!")
 main()
